b7eea3d5a8b267e39df873837c618ed6100aaca8
Cyrus Wyett <34195737+cjwyett@users.noreply.github.com>
2021-07-27 01:43:23 +0100

    Update upboot.sh
    kernel up

--- t420/kiss/upboot.sh
+++ t420/kiss/upboot.sh
@@ -8,10 +8,10 @@ if [ ! -f /boot/System.map ]; then
 	{ echo "System.map not found"; exit 1; } 
 fi
 
-mv /boot/vmlinuz-5.13.2 /boot/vmlinuz-5.13.2.old
-mv /boot/vmlinuz /boot/vmlinuz-5.13.2
-mv /boot/System.map-5.13.2  /boot/System.map-5.13.2.old
-mv /boot/System.map /boot/System.map-5.13.2
+mv /boot/vmlinuz-5.13.5 /boot/vmlinuz-5.13.5.old
+mv /boot/vmlinuz /boot/vmlinuz-5.13.5
+mv /boot/System.map-5.13.5  /boot/System.map-5.13.5.old
+mv /boot/System.map /boot/System.map-5.13.5
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=kiss 
 grub-mkconfig -o /boot/grub/grub.cfg
